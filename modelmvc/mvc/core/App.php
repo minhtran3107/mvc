c1cc8e33f2aae3baac6c9274e5088eb9951009c7
minhtran3107 <qtranvuminh@gmail.com>
2023-07-06 22:41:41 +0700

    first commit

---
+++ modelmvc/mvc/core/App.php
@@ -0,0 +1,49 @@
+<?php
+class App{
+
+    protected $controller="Home";
+    protected $action="SayHi" ;
+    protected $params=[];
+
+    function __construct(){
+        $arr = $this->urlProcess();
+
+        //xử lý controller
+        if(!empty($arr[0])){
+            if( file_exists('./mvc/controllers/'.$arr[0].'.php') ){
+                $this->controller = $arr[0];
+                unset($arr[0]);
+            }
+        }
+        require_once './mvc/controllers/'. $this->controller .'.php';
+        $this->controller = new $this->controller;
+
+        //Xử lý action
+        if(isset($arr[1])){
+            if(method_exists($this->controller, $arr[1])){
+                $this->action = $arr[1];
+            }
+            unset($arr[1]);
+        }
+
+        //Xử lý params
+        $this->params = $arr?array_values($arr):[];
+
+        // echo $this->controller.'</br>';
+        // echo $this->action.'</br>';
+        // print_r($this->params);
+
+        call_user_func_array([$this->controller, $this->action], $this->params );
+
+    }
+
+    function urlProcess(){
+        if(isset($_GET["url"])){
+            return explode("/", filter_var(trim($_GET["url"],"/")));
+        }
+    }
+
+}
+
+
+
